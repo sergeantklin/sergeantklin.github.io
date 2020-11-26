fc0b44c16ebbd4e6d017dfa72b3319c86dbbf818
Sergei Kalinkovskii <Sergei.Kalinkovskii@altenar.com>
2020-11-26 09:34:00 +0300

    Bootstrap added

---
+++ assets/js/closeNavBar.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var isNavbarOpened = $(".navbar-collapse").hasClass("show");
+        if (isNavbarOpened) {
+            $("button.navbar-toggler").click();
+        }
+    });
+});
